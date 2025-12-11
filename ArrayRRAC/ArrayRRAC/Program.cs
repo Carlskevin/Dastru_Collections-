a91d4367691aa7282c1a4914bd07aac06cda64c2
ACT-STUDENT <ACT-STUDENT@LAB2-PC45>
2025-12-11 16:26:35 +0800

    Oten

---
+++ ArrayRRAC/ArrayRRAC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRRAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;  
+            Console.WriteLine("Coach List: ");
+            Console.ResetColor();
+
+            string[] CoachNames = {"\nLoon", "Okay", "Cuyos", "Salvo", "Rodrigo" };
+            Array.Sort(CoachNames);
+
+            foreach (string Coachnames in CoachNames)
+            {
+                Console.WriteLine(Coachnames);
+            }
+            Console.ReadLine();
+        }
+    }
+}
