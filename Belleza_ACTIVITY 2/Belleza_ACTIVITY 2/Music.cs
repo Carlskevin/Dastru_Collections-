0f804f5d838ba397d15a2e89e5efd8faad336040
Carlskevin <carlskevinb@gmail.com>
2026-01-06 17:18:12 +0800

    Add files via upload
    ACTIVITY 2: Single Dimensional Array of Objects in OOP

---
+++ Belleza_ACTIVITY 2/Belleza_ACTIVITY 2/Music.cs
@@ -0,0 +1,72 @@
+﻿using Belleza_ACTIVITY_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belleza_ACTIVITY_2
+{
+    internal class Music
+    {
+        // Music Properties 
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Title { get; set; }
+
+        //Parameterized Constructor
+        public Music(string name, string genre, string title)
+        {
+            Name = name;
+            Genre = genre;
+            Title = title;
+        }
+        public virtual void DisplayInfo() //Virtual method      
+        {
+            Console.WriteLine($"Genre: {Genre}");
+            Console.WriteLine($"Title : {Title}");
+        }
+
+        }
+    }
+        // Derived Class 1
+        class PopMusic : Music
+        {
+            public string Artist { get; set; }   // get/set property
+
+            public PopMusic(string artist, string title)
+                : base(artist, "Pop", title)
+            {
+                Artist = artist;
+            }
+
+                public override void DisplayInfo()
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("POP MUSIC");
+                    Console.ResetColor();
+                    Console.WriteLine($"\nArtist: {Artist}");
+                    base.DisplayInfo();
+                }
+                    }
+                // Derived Class 2
+                class RockMusic : Music
+                {
+                    public string Band { get; set; }    // get/set property
+
+                    public RockMusic(string band, string title)
+                        : base(band, "Rock", title)
+                    {
+                        Band = band;
+                    }
+
+                    public override void DisplayInfo()
+                    {
+                        Console.ForegroundColor= ConsoleColor.Green;
+                        Console.WriteLine("ROCK MUSIC");
+                        Console.ResetColor();
+                        Console.WriteLine($"\nBand: {Band}");
+                        base.DisplayInfo();
+                    }
+
+                }
---
+++ Belleza_ACTIVITY 2/Belleza_ACTIVITY 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belleza_ACTIVITY_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Music[] musics = new Music[4];
+
+            //  Array of Music objects 
+            Music[] musicList = new Music[2];
+
+            musicList[0] = new PopMusic("Taylor Swift", "Blank Space");
+            musicList[1] = new RockMusic("Linkin Park", "Numb");
+
+            // Display music information
+            foreach (Music music in musicList)
+            {
+                music.DisplayInfo();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
