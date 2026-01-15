2afbf4017113ce3cfe5b32f3b4b217ed37c6b8a4
Carlskevin <carlskevinb@gmail.com>
2026-01-15 16:38:30 +0800

    Add files via upload

---
+++ Activity 4/2d array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create 2D Array Music
+            Music[,] musics = new Music[2,2];
+
+            //Row 2 
+            musics[0,0] = new PopMusic("Taylor Swift", "Blank Space");
+            musics[0,1] = new RockMusic("Linkin Park", "Numb");
+
+            //Row 1 
+            musics[1, 0] = new PopMusic("EXB", "Hayaan mo Sila");
+            musics[1, 1] = new RockMusic("Bamboo", "Noypi");
+
+            // Display music information
+            foreach (Music music in musics)
+            {
+                music.DisplayInfo();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
