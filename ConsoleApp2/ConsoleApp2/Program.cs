56bb422404d4148c9228c125efd71f853e99aab6
Carlskevin <carlskevinb@gmail.com>
2026-02-05 17:15:25 +0800

    ASYNC 2: Linked List Operations
    ASYNC 2: Linked List Operations

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctName = "okay";
+            string correctPassword = "123";
+            
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter your password:");
+                string password = Console.ReadLine();
+
+                if (name == correctName && password == correctPassword)
+                {
+                    Console.WriteLine("Welcome to the program!");
+                    break;
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Invalid credentials. Please try again.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
