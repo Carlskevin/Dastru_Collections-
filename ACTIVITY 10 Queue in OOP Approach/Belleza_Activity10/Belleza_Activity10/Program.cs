b6f69a0da90f51fbff70a8ce3288493af423354b
Carlskevin <carlskevinb@gmail.com>
2026-03-05 16:47:38 +0800

    ACTIVITY 10 Queue in OOP Approach
    ACTIVITY 10 Queue in OOP Approach

---
+++ ACTIVITY 10 Queue in OOP Approach/Belleza_Activity10/Belleza_Activity10/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belleza_Activity10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of QueueClass
+            Queue q = new Queue ();
+            int choice = 0;
+
+            // Loop for the menu until user chooses Exit
+            do
+            {
+                // Display menu
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n--- QUEUE MENU ---");
+                Console.ResetColor();
+                Console.WriteLine("\n1. Enqueue");
+                Console.WriteLine("2. Dequeue");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Display");
+                Console.WriteLine("5. Exit");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("\nEnter choice: ");
+                Console.ResetColor();
+
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        
+                        // Ask user for a number to insert in queue
+                        case 1:
+                            Console.Write("Enter number: ");
+                            int value = Convert.ToInt32(Console.ReadLine());
+                            q.Enqueue(value);
+                            break;
+
+                            // Call dequeue method to remove item
+                        case 2:
+                            q.Dequeue();
+                            break;
+                        
+                            // Call peek method to see the front item
+                        case 3:
+                            q.Peek();
+                            break;
+                        
+                            // Call display method to show queue items
+                        case 4:
+                            q.Display();
+                            break;
+                           
+                        case 5:
+                            Console.WriteLine("\nProgram Ended.");
+                            break;
+
+                        // If the user enters an invalid menu number
+                        default:
+                            Console.WriteLine("Invalid choice.");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    // Handle error if user enters letters instead of numbers
+                    Console.WriteLine("Invalid input! Please enter numbers only.");
+                }
+
+            } while (choice != 5);
+
+        }
+    }
+}
---
+++ ACTIVITY 10 Queue in OOP Approach/Belleza_Activity10/Belleza_Activity10/Queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belleza_Activity10
+{
+    internal class Queue
+    {
+
+        // Create an array to store queue elements
+        private int[] queue = new int[5];
+        private int front = -1;
+        private int rear = -1;
+
+        // Method to add an item to the queue
+        public void Enqueue(int value)
+        {
+            if (rear == queue.Length - 1)
+            {
+                Console.WriteLine("Queue Overflow! Queue is full.");
+            }
+            else
+            {
+                if (front == -1)
+                    front = 0;
+
+                rear++;
+                queue[rear] = value;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\nItem inserted successfully.");
+                Console.ResetColor();
+
+            }
+        }
+
+        // Method to remove an item from the queue
+        public void Dequeue()
+        {
+            if (front == -1 || front > rear)
+            {
+                Console.WriteLine("Queue Underflow! Queue is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Removed item: " + queue[front]);
+                front++;
+            }
+        }
+
+        // Method to check the front item
+        public void Peek()
+        {
+            if (front == -1 || front > rear)
+            {
+                Console.WriteLine("Queue is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Front item: " + queue[front]);
+            }
+        }
+
+        //Method to display all queue elements
+        public void Display()
+        {
+            if (front == -1 || front > rear)
+            {
+                Console.WriteLine("Queue is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Queue elements:");
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+            }
+
+        }
+    }
+}
